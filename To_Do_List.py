e879752b1dcee6a88eb9730739526a90820dc0f0
unknown <rifqingf950@gmail.com>
2025-11-23 22:43:49 +0700

    Added second menu function

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,17 +1,26 @@
 import os
 import pandas as pd
 
-if os.path.getsize("lists.csv") > 0:
-    df = pd.read_csv("lists.csv")
-else:
-    df = pd.DataFrame()
-df.index = df.index + 1
+df = pd.read_csv("lists.csv")
 
 def FirstMenu():
     if df.empty:
         print("Empty plan")
     else:
         print(df)
+
+def SecondMenu():
+    plan = input("Enter the plan: ")
+    while True:
+        time = input("Enter the time: ")
+        if time.isdigit():
+            df = pd.read_csv("lists.csv")
+            newplan = pd.DataFrame([{"time": time, "plan": plan}])
+            df = pd.concat([df, newplan], ignore_index=False)
+            df.to_csv("lists.csv", index=False)
+            break
+        else:
+            print("error: use digits for the time")
 
 #Open main menu
 print("What would you like to do today?")
@@ -24,6 +33,8 @@ while True:
         index = int(index)
         if index == 1:
             FirstMenu()
+        elif index == 2:
+            SecondMenu()
         else:
             print("error: invalid index number inputted")
     else:
